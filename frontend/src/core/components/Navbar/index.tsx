e30d136f347021a3785f8c8bbcb4304c92b5bb29
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-07-27 21:19:49 -0300

    Add Navbar

--- frontend/src/core/components/Navbar/index.tsx
+++ frontend/src/core/components/Navbar/index.tsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import { ReactComponent as Logo } from 'core/assets/images/logo.svg'
+import './styles.scss'
 
 const Navbar = () => {
     return (
-        <nav className="row bg-primary main-nav">
+        <nav className="row main-nav">
             <div className="col-3">
                 <Logo />
             </div>
             <div className="col-6">
                 <ul className="main-menu">
                     <li>
-                        Diárias
+                        <a href="link" className="nav-link active">
+                            Diárias
+                        </a>
                     </li>
                     <li>
-                        Entradas
+                        <a href="link" className="nav-link">
+                            Entradas
+                        </a>
                     </li>
                     <li>
-                        Estoque
+                        <a href="link" className="nav-link">
+                            Estoque
+                        </a> 
                     </li>
                 </ul>
             </div>
-            <div className="col-3 text-end">
+            <div className="col-3 text-end ">
                 Olá Leandro!
             </div>
         </nav>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import 'bootstrap/dist/css/bootstrap.css';
 
+ReactDOM.render(
+  <App />,
+document.getElementById('root')
+);
