16cecbf671902ae9415b2eff98608efcb323e9aa
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-07-28 23:47:06 -0300

    Fix EmployeeFilter

--- frontend/src/pages/Daily/EmployeeFilter/index.tsx
+++ frontend/src/pages/Daily/EmployeeFilter/index.tsx
@@ -1,21 +1,20 @@
+import React from 'react'
 import Select from 'core/components/Select'
 import Table from 'core/components/Table'
-import React from 'react'
 
 const EmployeeFilter = () => (
-    <div className="filter-client">
+    <div className="filter-employee">
         <Select filter='Funcionário'/>
         <div className="table-click">
             <Table
                 name="Antônio da Melo de Conceição Vieira"
                 admissionDate="28/07/2021"
-                cnpj="22.123.000/1500-00"
                 cpf="123.195.299-12"
-                corporateName="Citrus"
-                address="Rua Joaquim Filadéfia, Avenida das flores, nº153 CEP: 400500-000"
+                status="ativo"
             />
         </div>
     </div>
 )
+
 
 export default EmployeeFilter
--- frontend/src/pages/Daily/index.tsx
+++ frontend/src/pages/Daily/index.tsx
@@ -1,18 +1,8 @@
 import React from "react";
-import Select from "core/components/Select";
-import Table from "core/components/Table";
+
 
 const Daily = () => (
-    <div className="filter-client">
-        <Select filter='Funcionário'/>
-        <div className="table-click">
-            <Table
-                name="Antônio da Melo de Conceição Vieira"
-                admissionDate="28/07/2021"
-                cpf="123.195.299-12"
-                status="ativo"
-            />
-        </div>
+    <div>
     </div>
 )
 
