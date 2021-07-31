ec7ecd361e3d70e0c03e04f6a2ed1a7680439c8a
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-07-30 21:16:28 -0300

    Refactoring DateFilter

--- frontend/src/core/components/DateFilter/index.tsx
+++ frontend/src/core/components/DateFilter/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, KeyboardDateTimePicker } from '@material-ui/pickers';
-import './styles.scss'
 
 const DateFilter = () => {
   const [selectedDate, setSelectedDate] = React.useState<Date | null>(
@@ -13,9 +12,7 @@ const DateFilter = () => {
   };
 
   return (
-    <div className="date-filter input-filter">
-      <div className="from">
-        <span>De:</span>
+    <div className="">
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <div>
             <KeyboardDateTimePicker
@@ -33,28 +30,6 @@ const DateFilter = () => {
             />
           </div>
         </MuiPickersUtilsProvider>
-      </div>
-      <div className='to'>
-        <span>Até:</span>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <div>
-            <KeyboardDateTimePicker
-              className='date-filter'
-              disableToolbar
-              variant="inline"
-              format="dd/MM/yyyy"
-              margin="normal"
-              id="date-picker-inline"
-              label=''
-              value={selectedDate}
-              onChange={handleDateChange}
-              KeyboardButtonProps={{
-                'aria-label': 'change date',
-              }}
-            />
-          </div>
-        </MuiPickersUtilsProvider>
-      </div>
     </div>
   )
 }
--- frontend/src/pages/Daily/index.tsx
+++ frontend/src/pages/Daily/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Select from "core/components/Select";
-import Table from "core/components/Table";
 import DateFilter from "core/components/DateFilter";
 
 import './styles.scss'
@@ -10,20 +9,83 @@ const Daily = () => (
   <div className="general-filter">
     <div className="general-filter-types">
       <Select filter="Funcionário" />
-      <DateFilter />
+      <div className="date-filter input-filter">
+        <div className="from">
+          <span>De:</span>
+          <DateFilter />
+        </div>
+        <div className='to'>
+          <span>Até:</span>
+          <DateFilter />
+        </div>
+      </div>
+
       <Select filter="Fornecedor" />
       <Select filter="Cliente" />
       <div className="checkbox-input-filter input-filter">
-        <CheckboxFilter nameFilter="Pendente"/>
-        <CheckboxFilter nameFilter="Resolvido"/>
+        <CheckboxFilter nameFilter="Pendente" />
+        <CheckboxFilter nameFilter="Resolvido" />
       </div>
     </div>
-    <Table
-      name="Antônio da Melo de Conceição Vieira"
-      admissionDate="28/07/2021"
-      cpf="123.195.299-12"
-      status="ativo"
-    />
+    <table className="table-default">
+      <thead>
+        <tr>
+          <th>Funcionário</th>
+          <th>Data</th>
+          <th>Unidade</th>
+          <th>Fornecedor</th>
+          <th>Cliente</th>
+          <th>Preço</th>
+          <th>Total</th>
+          <th>Obervação</th>
+          <th>Situação</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <div className="table-line">
+              Maria
+            </div>
+          </td>
+          <td>
+            <div className="table-line">
+              <DateFilter />
+            </div>
+          </td>
+          <td>
+            <div className="table-line">
+              120,52kg
+            </div>
+          </td>
+          <td>
+            <div className="table-line">
+              Antonio
+            </div>
+          </td>
+          <td>
+            <div className="table-line">
+              Clayton
+            </div>
+          </td>
+          <td>
+            <div className="table-line">
+              R$0,50
+            </div>
+          </td>
+          <td>
+            <div className="table-line">
+              Hoje fez muito frio
+            </div>
+          </td>
+          <td>
+            <div className="table-line">
+              Pendente
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div >
 )
 
