3e6fa1e8dc479ee094626275db7a37a4fdcc7da3
Atila Silva <atila__7@hotmail.com>
2021-08-14 22:21:25 -0300

    Find provider by id

--- backend/src/main/java/com/atmat/sua/services/ProviderService.java
+++ backend/src/main/java/com/atmat/sua/services/ProviderService.java
@@ -1,10 +1,12 @@
 package com.atmat.sua.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.atmat.sua.dto.ProviderDTO;
 import com.atmat.sua.entities.Provider;
@@ -16,9 +18,16 @@ public class ProviderService {
 	@Autowired
 	private ProviderRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<ProviderDTO> findAll(){
 		List<Provider> list = repository.findAll();
 		return list.stream().map(x -> new ProviderDTO(x)).collect(Collectors.toList());
 	}
-	
+
+	@Transactional(readOnly = true)
+	public ProviderDTO findById(Long id) {
+		Optional<Provider> obj = repository.findById(id);
+		Provider entity = obj.get();
+		return new ProviderDTO(entity);
+	}
 }
