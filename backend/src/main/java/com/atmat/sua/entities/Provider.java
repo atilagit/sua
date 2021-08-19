054fd482fb505fda33a05827b35f69db510f3f24
Atila Silva <atila__7@hotmail.com>
2021-08-18 21:46:35 -0300

    Auditing data

--- backend/src/main/java/com/atmat/sua/entities/Provider.java
+++ backend/src/main/java/com/atmat/sua/entities/Provider.java
@@ -1,15 +1,19 @@
 package com.atmat.sua.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -27,6 +31,12 @@ public class Provider implements Serializable {
 	
 	@OneToMany(mappedBy = "provider")
 	List<Posting> postings = new ArrayList<>();
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	
 	public Provider() {
 	}
@@ -87,6 +97,24 @@ public class Provider implements Serializable {
 
 	public void setPostings(List<Posting> postings) {
 		this.postings = postings;
+	}
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
