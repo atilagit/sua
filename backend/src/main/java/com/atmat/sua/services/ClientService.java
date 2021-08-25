ed960114631434bc4c5cc7bbc04612e89bda7644
Atila Silva <atila__7@hotmail.com>
2021-08-25 19:50:03 -0300

    Delete Client

--- backend/src/main/java/com/atmat/sua/services/ClientService.java
+++ backend/src/main/java/com/atmat/sua/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ import com.atmat.sua.entities.Address;
 import com.atmat.sua.entities.Client;
 import com.atmat.sua.entities.repositories.AddressRepository;
 import com.atmat.sua.entities.repositories.ClientRepository;
+import com.atmat.sua.services.exceptions.DatabaseException;
 import com.atmat.sua.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -57,6 +60,16 @@ public class ClientService {
 			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id " + id + " not found");
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found");
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 	
