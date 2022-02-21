b0e257d98ab6a41c032a9b4913932ba2cef278f3
Atila Silva <atila__7@hotmail.com>
2022-02-21 19:20:51 -0300

    Prettier - Code formatter

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,8 @@
-import './assets/styles/custom.scss'
+import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
-  return (
-    <h1>Hello SUA</h1>
-  );
+  return <h1>Hello SUA</h1>;
 }
 
 export default App;
