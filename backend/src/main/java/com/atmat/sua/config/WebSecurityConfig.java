67ed1fb89ddf7b24d5e40c04c4927250bdf0acb0
Atila Silva <atila__7@hotmail.com>
2022-05-19 07:17:45 -0300

    Rollback web security config

--- backend/src/main/java/com/atmat/sua/config/WebSecurityConfig.java
+++ backend/src/main/java/com/atmat/sua/config/WebSecurityConfig.java
@@ -33,11 +33,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-	}
-
-	@Override
 	@Bean
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
