693e10f9ba8c85884154322ccde6ad3047b823cc
Atila Silva <atila__7@hotmail.com>
2022-05-23 20:19:12 -0300

    Filter posting by date - Parte 2 (resolvendo imcompatibilidade com o postigres

--- backend/src/main/java/com/atmat/sua/repositories/PostingRepository.java
+++ backend/src/main/java/com/atmat/sua/repositories/PostingRepository.java
@@ -21,6 +21,6 @@ public interface PostingRepository extends JpaRepository<Posting, Long>{
 			+ "(:client IS NULL OR :client = obj.client) AND "
 			+ "(:provider IS NULL OR :provider = obj.provider) AND "
 			+ "(:resolved IS NULL OR :resolved = obj.resolved) AND "
-			+ "(:de IS NULL OR obj.date BETWEEN :de AND :ate)")
+			+ "(cast(:de as date) IS NULL OR obj.date BETWEEN :de AND :ate)")
 	Page<Posting> find(Employee employee, Client client, Provider provider, Boolean resolved, LocalDate de, LocalDate ate, Pageable pageable);
 }
