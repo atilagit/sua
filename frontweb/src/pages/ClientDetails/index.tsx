27e17986d90979bbbd78b818b5d0ae1826eeba1e
Atila Silva <atila__7@hotmail.com>
2022-05-26 08:51:50 -0300

    Integration with backend - Client

--- frontweb/src/pages/ClientDetails/index.tsx
+++ frontweb/src/pages/ClientDetails/index.tsx
@@ -4,74 +4,73 @@ import Footer from 'components/Footer';
 import { Client } from 'types/client';
 import { formatCEP, formatCpfCnpj } from 'util/formatters';
 
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import './styles.css';
+import axios from 'axios';
+import { BASE_URL } from 'util/requests';
+import { useEffect, useState } from 'react';
+
+type UrlParams = {
+    clientId: string;
+}
 
 const ClientDetails = () => {
 
-    const client: Client = {
-        "id": 1,
-        "contact": "Jonielson Cleibisson Cleber e Kauan da Silva",
-        "corporateName": "Citrus e Cia",
-        "cpf": "97717222041",
-        "cnpj": "18107334000121",
-        "active": true,
-        "address": {
-            "id": 1,
-            "street": "Rua da Confibra",
-            "number": "541",
-            "neighborhood": "Jardim Campos Verdes",
-            "complement": "Exemplo de complemento",
-            "city": "Hortolândia",
-            "state": "SP",
-            "cep": "13186070"
-        }
-    }
+    const { clientId } = useParams<UrlParams>();
+
+    const [client, setClient] = useState<Client>();
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/clients/${clientId}`)
+            .then(response => {
+                setClient(response.data)
+            });
+    }, [clientId]);
 
     return (
         <div className="page-container page-container-especific">
           <div className="title-content-container">
             <h1>
-                <Link to="/">Início</Link> / <Link to="/clients"> Clientes </Link> / {client.contact}
+                <Link to="/">Início</Link> / <Link to="/clients"> Clientes </Link> / {client?.contact}
             </h1>
           </div>
           <div className='fields-container'>
             <div className='col5-466'>
-                <FieldDetailCard title = "Nome do Contato" content = {client.contact}/>
+                {client && <FieldDetailCard title = "Nome do Contato" content = {client.contact}/>}
             </div>
             <div className='col2-178'>
-                <FieldDetailCard title = "CNPJ" content = {formatCpfCnpj(client.cnpj)}/>
+                {client && <FieldDetailCard title = "CNPJ" content = {(client.cnpj != null) ? formatCpfCnpj(client.cnpj) : ""}/>}
             </div>
             <div className='col2-178'>
-                <FieldDetailCard title = "CPF" content = {formatCpfCnpj(client.cpf)}/>
+                {client && <FieldDetailCard title = "CPF" content = {(client.cpf != null) ? formatCpfCnpj(client.cpf) : ""}/>}
             </div>
             <div className='col3-274'>
-                <FieldDetailCard title = "Razão social" content = {client.corporateName}/>
+                {client && <FieldDetailCard title = "Razão social" content = {client.corporateName}/>}
             </div>
             <div className='col5-466'>
-                <FieldDetailCard title = "Rua" content = {client.address.street}/>
+                {client && <FieldDetailCard title = "Rua" content = {(client.address != null) ? client.address.street : ""}/>}
             </div>
             <div className='col1-82'>
-                <FieldDetailCard title = "Nº" content = {client.address.number}/>
+                {client && <FieldDetailCard title = "Nº" content = {(client.address != null) ? client.address.number : ""}/>}
             </div>
             <div className='col3-274'>
-                <FieldDetailCard title = "Bairro" content = {client.address.neighborhood}/>
+                {client && <FieldDetailCard title = "Bairro" content = {(client.address != null) ? client.address.neighborhood : ""}/>}
             </div>
             <div className='col3-274'>
-                <FieldDetailCard title = "Cidade" content = {client.address.city}/>
+                {client && <FieldDetailCard title = "Cidade" content = {(client.address != null) ? client.address.city : ""}/>}
             </div>
             <div className='col3-274'>
-                <FieldDetailCard title = "Estado" content = {client.address.state}/>
+                {client && <FieldDetailCard title = "Estado" content = {(client.address != null) ? client.address.state : ""}/>}
             </div>
             <div className='col2-178'>
-                <FieldDetailCard title = "CEP" content = {formatCEP(client.address.cep)}/>
+                {client && <FieldDetailCard title = "CEP" content = {(client.address != null && client.address.cep != null) ? formatCEP(client.address.cep) : ""}/>}
             </div>
             <div className='col5-466'>
-                <FieldDetailCard title = "Comlemento" content = {client.address.complement}/>
+                {client && <FieldDetailCard title = "Comlemento" content = {(client.address != null) ? client.address.complement : ""}/>}
             </div>
             <div className='col2-178'>
-                <FieldDetailCard title = "Status" content = {client.active ? "Ativo" : "Inativo"}/>
+                <FieldDetailCard title = "Status" content = {client?.active ? "Ativo" : "Inativo"}/>
             </div>
           </div>
           <div className='buttons-container'>
