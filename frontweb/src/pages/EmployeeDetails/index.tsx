09b5a6fac0c31638a585c43011105b3956f7f980
Atila Silva <atila__7@hotmail.com>
2022-05-26 16:09:30 -0300

    Integration with backend - Employee

--- frontweb/src/pages/EmployeeDetails/index.tsx
+++ frontweb/src/pages/EmployeeDetails/index.tsx
@@ -4,13 +4,31 @@ import Footer from "components/Footer";
 import { Employee } from "types/employee";
 import { formatCEP, formatCpfCnpj, formatDate } from "util/formatters";
 
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import './styles.css';
+import axios from 'axios';
+import { BASE_URL } from 'util/requests';
+import { useEffect, useState } from 'react';
+
+type UrlParams = {
+    employeeId: string;
+}
 
 const EmployeeDetails = () => {
 
-    const employee: Employee = {
+    const { employeeId } = useParams<UrlParams>();
+
+    const [employee, setEmployee] = useState<Employee>();
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/employees/${employeeId}`)
+            .then(response => {
+                setEmployee(response.data)
+            });
+    }, [employeeId]);
+
+    /*const employee: Employee = {
         "id": 1,
         "name": "Leandro Santos Silva Pereira Inácio",
         "cpf": "39856849978",
@@ -41,51 +59,51 @@ const EmployeeDetails = () => {
                 "authority": "ROLE_OPERATOR"
             }
         ]
-    }
+    }*/
 
     return (
         <div className="page-container page-container-especific">
           <div className="title-content-container">
             <h1>
-                <Link to="/">Início</Link> / <Link to="/employees"> Funcionários </Link> / {employee.name}
+                <Link to="/">Início</Link> / <Link to="/employees"> Funcionários </Link> / {employee?.name}
             </h1>
           </div>
           <div className='fields-container'>
             <div className='col5-466'>
-                <FieldDetailCard title = "Nome do Contato" content = {employee.name}/>
+                {employee && <FieldDetailCard title = "Nome do Contato" content = {employee.name}/>}
             </div>
             <div className='col2-178'>
-                <FieldDetailCard title = "Data Admissão" content = {formatDate(employee.admissionDate)}/>
+                {employee && <FieldDetailCard title = "Data Admissão" content = {(employee.admissionDate != null) ? formatDate(employee.admissionDate) : ""}/>}
             </div>
             <div className='col2-178'>
-                <FieldDetailCard title = "CPF" content = {formatCpfCnpj(employee.cpf)}/>
+                {employee && <FieldDetailCard title = "CPF" content = {(employee.cpf != null) ? formatCpfCnpj(employee.cpf) : ""}/>}
             </div>
             <div className='col3-274'>
-                <FieldDetailCard title = "Acesso" content = {employee.roles.length > 2? "Administrador" : employee.roles.length > 1? "Operador" : "Básico"}/>
+                {employee && <FieldDetailCard title = "Acesso" content = {employee.roles.length > 2? "Administrador" : employee.roles.length > 1? "Operador" : "Básico"}/>}
             </div>
             <div className='col5-466'>
-                <FieldDetailCard title = "Rua" content = {employee.address.street}/>
+                {employee && <FieldDetailCard title = "Rua" content = {(employee.address != null) ? employee.address.street : ""}/>}
             </div>
             <div className='col1-82'>
-                <FieldDetailCard title = "Nº" content = {employee.address.number}/>
+                {employee && <FieldDetailCard title = "Nº" content = {(employee.address != null) ? employee.address.number : ""}/>}
             </div>
             <div className='col3-274'>
-                <FieldDetailCard title = "Bairro" content = {employee.address.neighborhood}/>
+                {employee && <FieldDetailCard title = "Bairro" content = {(employee.address != null) ? employee.address.neighborhood : ""}/>}
             </div>
             <div className='col3-274'>
-                <FieldDetailCard title = "Cidade" content = {employee.address.city}/>
+                {employee && <FieldDetailCard title = "Cidade" content = {(employee.address != null) ? employee.address.city : ""}/>}
             </div>
             <div className='col3-274'>
-                <FieldDetailCard title = "Estado" content = {employee.address.state}/>
+                {employee && <FieldDetailCard title = "Estado" content = {(employee.address != null) ? employee.address.state : ""}/>}
             </div>
             <div className='col2-178'>
-                <FieldDetailCard title = "CEP" content = {formatCEP(employee.address.cep)}/>
+                {employee && <FieldDetailCard title = "CEP" content = {(employee.address != null && employee.address.cep != null) ? formatCEP(employee.address.cep) : ""}/>}
             </div>
             <div className='col5-466'>
-                <FieldDetailCard title = "Comlemento" content = {employee.address.complement}/>
+                {employee && <FieldDetailCard title = "Comlemento" content = {(employee.address != null) ? employee.address.complement : ""}/>}
             </div>
             <div className='col2-178'>
-                <FieldDetailCard title = "Status" content = {employee.active ? "Ativo" : "Inativo"}/>
+                <FieldDetailCard title = "Status" content = {employee?.active ? "Ativo" : "Inativo"}/>
             </div>
           </div>
           <div className='buttons-container'>
