42c4491229ff547a9983d5fb982d821c179eaec1
Atila Silva <atila__7@hotmail.com>
2022-05-27 07:31:22 -0300

    Remove commented code

--- frontweb/src/pages/EmployeeDetails/index.tsx
+++ frontweb/src/pages/EmployeeDetails/index.tsx
@@ -28,39 +28,6 @@ const EmployeeDetails = () => {
             });
     }, [employeeId]);
 
-    /*const employee: Employee = {
-        "id": 1,
-        "name": "Leandro Santos Silva Pereira Inácio",
-        "cpf": "39856849978",
-        "admissionDate": "2018-10-18",
-        "login": "leandro@gmail.com",
-        "active": true,
-        "address": {
-          "id": 2,
-          "street": "Rua Alessandro Bonci",
-          "number": "520",
-          "neighborhood": "Eldorado",
-          "complement": "Perto do posto Ipiranga",
-          "city": "São Paulo",
-          "state": "SP",
-          "cep": "04476280"
-          },
-        "roles": [
-            {
-                "id": 3,
-                "authority": "ROLE_ADMIN"
-            },
-            {
-                "id": 1,
-                "authority": "ROLE_BASIC"
-            },
-            {
-                "id": 2,
-                "authority": "ROLE_OPERATOR"
-            }
-        ]
-    }*/
-
     return (
         <div className="page-container page-container-especific">
           <div className="title-content-container">
