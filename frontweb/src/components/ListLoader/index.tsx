910ac19c443a0d01a8d99337cf3f90fa84b70885
Atila Silva <atila__7@hotmail.com>
2022-06-02 08:08:56 -0300

    Fix Postings Loader

--- frontweb/src/components/ListLoader/index.tsx
+++ frontweb/src/components/ListLoader/index.tsx
@@ -5,19 +5,19 @@ const ListLoader = () => {
     <ContentLoader
       speed={2}
       width={1400}
-      height={850}
-      viewBox="0 0 1400 850"
+      height={900}
+      viewBox="0 0 1400 900"
       backgroundColor="#c8c8c8"
       foregroundColor="#ecebeb"
     >
-      <rect x="0" y="15" rx="5" ry="5" width="1130" height="85" />
-      <rect x="0" y="115" rx="5" ry="5" width="1130" height="85" />
-      <rect x="0" y="215" rx="5" ry="5" width="1130" height="85" />
-      <rect x="0" y="315" rx="5" ry="5" width="1130" height="85" />
-      <rect x="0" y="415" rx="5" ry="5" width="1130" height="85" />
-      <rect x="0" y="515" rx="5" ry="5" width="1130" height="85" />
-      <rect x="0" y="615" rx="5" ry="5" width="1130" height="85" />
-      <rect x="0" y="715" rx="5" ry="5" width="1130" height="85" />
+      <rect x="0" y="0" rx="25" ry="25" width="1120" height="94" />
+      <rect x="0" y="110" rx="25" ry="25" width="1120" height="94" />
+      <rect x="0" y="220" rx="25" ry="25" width="1120" height="94" />
+      <rect x="0" y="330" rx="25" ry="25" width="1120" height="94" />
+      <rect x="0" y="440" rx="25" ry="25" width="1120" height="94" />
+      <rect x="0" y="550" rx="25" ry="25" width="1120" height="94" />
+      <rect x="0" y="660" rx="25" ry="25" width="1120" height="94" />
+      <rect x="0" y="770" rx="25" ry="25" width="1120" height="94" />
     </ContentLoader>
   )
 }
