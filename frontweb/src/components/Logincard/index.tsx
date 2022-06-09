6b557112f09a77c9b42e263c8b63b0b079b97117
Atila Silva <atila__7@hotmail.com>
2022-06-08 22:33:34 -0300

    React Hook Form - First Test

--- frontweb/src/components/Logincard/index.tsx
+++ frontweb/src/components/Logincard/index.tsx
@@ -1,23 +1,31 @@
 import Button from 'components/Button';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
-type Props = {
-  text: String;
-  btn4: string;
-  btn4rota: string;
+type FormData = {
+  username: string;
+  password: string;
 }
 
-const Logincard = ({ text, btn4, btn4rota }: Props) => {
+const Logincard = () => {
+
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (formData : FormData) => {
+    console.log(formData);
+  };
+
   return (
     <>
       <div className="base-card auth-card">
         <div className="auth-content-container">
-          <h2>{text}</h2>
+          <h2>LOGIN</h2>
         </div>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4 mt-4">
             <input
+              {...register("username")}
               type="text"
               className="form-control base-input"
               placeholder="Email ou CPF"
@@ -26,6 +34,7 @@ const Logincard = ({ text, btn4, btn4rota }: Props) => {
           </div>
           <div className="mb-1">
             <input
+              {...register("password")}
               type="password"
               className="form-control base-input "
               placeholder="Password"
--- frontweb/src/pages/Auth/index.tsx
+++ frontweb/src/pages/Auth/index.tsx
@@ -1,20 +1,61 @@
-import Footer from "components/Footer";
-import Logincard from "components/Logincard";
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
+import Button from 'components/Button';
 
 import './styles.css';
+import Footer from "components/Footer";
+
+type FormData = {
+  username: string;
+  password: string;
+}
 
 const Auth = () => {
-    return (
-        <>
-          <div className="auth-container">
-            <Logincard
-              text="LOGIN" 
-              btn4="ENTRAR" btn4rota="/"
+
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (formData : FormData) => {
+    console.log(formData);
+  };
+
+  return (
+    <>
+    <div className="auth-container">
+      <div className="base-card auth-card">
+        <div className="auth-content-container">
+          <h2>LOGIN</h2>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4 mt-4">
+            <input
+              {...register("username")}
+              type="text"
+              className="form-control base-input"
+              placeholder="Email ou CPF"
+              name="username"
             />
           </div>
-          <Footer />
-        </>
-      );
+          <div className="mb-1">
+            <input
+              {...register("password")}
+              type="password"
+              className="form-control base-input "
+              placeholder="Password"
+              name="password"
+            />
+          </div>
+          <Link to="/auth/recover" className="login-link-recover">
+            Esqueci a senha
+          </Link>
+          <div className="auth-button-container">
+            <Button text="ENTRAR" />
+          </div>
+        </form>
+      </div>
+    </div>
+    <Footer />
+    </>
+  );
 }
 
 export default Auth;
