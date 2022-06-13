7e6f368db859c8bc79a0ff6c042cc3c4afb83117
Atila Silva <atila__7@hotmail.com>
2022-06-13 20:44:44 -0300

    Manual error handling

--- frontweb/src/pages/Auth/index.tsx
+++ frontweb/src/pages/Auth/index.tsx
@@ -5,6 +5,7 @@ import Button from 'components/Button';
 import './styles.css';
 import Footer from "components/Footer";
 import { requestBackendLogin } from "util/requests";
+import { useState } from "react";
 
 type FormData = {
   username: string;
@@ -13,54 +14,63 @@ type FormData = {
 
 const Auth = () => {
 
+  const [hasError, setHasError] = useState(false);
+
   const { register, handleSubmit } = useForm<FormData>();
 
-  const onSubmit = (formData : FormData) => {
+  const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
-    .then(response => {
-      console.log('SUCESSO', response);
-    })
-    .catch(error => {
-      console.log('ERRO', error);
-    });
+      .then(response => {
+        setHasError(false);
+        console.log('SUCESSO', response);
+      })
+      .catch(error => {
+        setHasError(true);
+        console.log('ERRO', error);
+      });
   };
 
   return (
     <>
-    <div className="auth-container">
-      <div className="base-card auth-card">
-        <div className="auth-content-container">
-          <h2>LOGIN</h2>
+      <div className="auth-container">
+        <div className="base-card auth-card">
+          <div className="auth-content-container">
+            <h2>LOGIN</h2>
+            {hasError && (
+              <div className="alert alert-danger">
+                Erro ao tentar efetuar login.
+              </div>
+            )}
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-4 mt-4">
+              <input
+                {...register("username")}
+                type="text"
+                className="form-control base-input"
+                placeholder="Email ou CPF"
+                name="username"
+              />
+            </div>
+            <div className="mb-1">
+              <input
+                {...register("password")}
+                type="password"
+                className="form-control base-input "
+                placeholder="Password"
+                name="password"
+              />
+            </div>
+            <Link to="/auth/recover" className="login-link-recover">
+              Esqueci a senha
+            </Link>
+            <div className="auth-button-container">
+              <Button text="ENTRAR" />
+            </div>
+          </form>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-4 mt-4">
-            <input
-              {...register("username")}
-              type="text"
-              className="form-control base-input"
-              placeholder="Email ou CPF"
-              name="username"
-            />
-          </div>
-          <div className="mb-1">
-            <input
-              {...register("password")}
-              type="password"
-              className="form-control base-input "
-              placeholder="Password"
-              name="password"
-            />
-          </div>
-          <Link to="/auth/recover" className="login-link-recover">
-            Esqueci a senha
-          </Link>
-          <div className="auth-button-container">
-            <Button text="ENTRAR" />
-          </div>
-        </form>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 }
