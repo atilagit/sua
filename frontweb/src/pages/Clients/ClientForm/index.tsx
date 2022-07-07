791ce503692d35f4d071e553b5cddc281001f6cf
Atila Silva <atila__7@hotmail.com>
2022-07-06 21:24:21 -0300

    Front - Clients Crud part 2

--- frontweb/src/pages/Clients/ClientForm/index.tsx
+++ frontweb/src/pages/Clients/ClientForm/index.tsx
@@ -1,8 +1,56 @@
-
+import './styles.css';
 
 const ClientForm = () => {
     return (
-        <h1>form client</h1>
+        <div className="page-container form-client-page-container-especific">
+            <div className="form-client-titulo-container">
+                <h1>Cadastro de Cliente</h1>
+            </div>
+            <form action=''>
+                <div className='form-client-fields-container'>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col5-466' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col2-178' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col2-178' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col3-274' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col5-466' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col1-82' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col3-274' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col3-274' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col3-274' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col2-178' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col5-466' />
+                    </div>
+                    <div>
+                        <input type="text" className='form-control base-card form-client-field form-client-col2-178' />
+                    </div>
+                </div>
+                <div className='client-crud-buttons-container'>
+                    <button className='btn clients-cancel-button'>CANCELAR</button>
+                    <button className='btn clients-save-button'>SALVAR</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
