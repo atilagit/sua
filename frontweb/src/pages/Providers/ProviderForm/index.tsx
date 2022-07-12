ba8f253447bdf01fde8d1d641b79304282691654
Atila Silva <atila__7@hotmail.com>
2022-07-11 22:24:11 -0300

    Front - Providers Crud part 3

--- frontweb/src/pages/Providers/ProviderForm/index.tsx
+++ frontweb/src/pages/Providers/ProviderForm/index.tsx
@@ -1,52 +1,162 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { Provider } from 'types/provider';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const ProviderForm = () => {
+
+    const history = useHistory();
+
+    const { register, handleSubmit, formState: { errors } } = useForm<Provider>();
+
+    const onSubmit = (formData: Provider) => {
+
+        const config: AxiosRequestConfig = {
+            method: 'POST',
+            url: "/providers",
+            data: formData,
+            withCredentials: true
+        }
+
+        requestBackend(config)
+            .then(response => {
+                console.log(response.data);
+            });
+    };
+
+    const handleCancel = () => {
+        history.replace("/providers");
+    }
+
     return (
         <div className="page-container form-provider-page-container-especific">
             <div className="form-provider-titulo-container">
                 <h1>Cadastro de Fornecedor</h1>
             </div>
-            <form action=''>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className='form-provider-fields-container'>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col5-466'/>
+                        <label about='name'>Nome do Contato*</label>
+                        <input
+                            {...register("name", {
+                                required: 'Campo obrigatório'
+                            })}
+                            type="text"
+                            className={`form-control base-card form-provider-field form-provider-col5-466 ${errors.name ? 'is-invalid' : ''}`}
+                            placeholder="Ex: João Batista da Silva"
+                            name="name"
+                        />
+                        <div className="invalid-feedback d-block">{errors.name?.message}</div>
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col2-178'/>
+                        <label about='cnpj'>CNPJ</label>
+                        <input
+                            {...register("cnpj")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col2-178'
+                            placeholder="00.000.000/0000-00"
+                            name="cnpj"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col2-178'/>
+                        <label about='cpf'>CPF</label>
+                        <input 
+                            {...register("cpf")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col2-178'
+                            placeholder="000.000.000-00"
+                            name="cpf"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col3-274'/>
+                        <label about='corporateName'>Razão Social</label>
+                        <input 
+                            {...register("corporateName")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col3-274' 
+                            name="corporateName"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col5-466'/>
+                        <label about='address.street'>Rua</label>
+                        <input 
+                            {...register("address.street")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col5-466' 
+                            name="address.street"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col1-82'/>
+                        <label about='number'>Nº</label>
+                        <input 
+                            {...register("address.number")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col1-82' 
+                            name="address.number"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col3-274' />
+                        <label about='neighborhood'>Bairro</label>
+                        <input 
+                            {...register("address.neighborhood")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col3-274' 
+                            name="address.neighborhood"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col3-274' />
+                        <label about='city'>Cidade</label>
+                        <input 
+                            {...register("address.city")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col3-274' 
+                            name="address.city"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col3-274'/>
+                        <label about='state'>Estado</label>
+                        <input 
+                            {...register("address.state")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col3-274' 
+                            name="address.state"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col2-178'/>
+                        <label about='cep'>CEP</label>
+                        <input 
+                            {...register("address.cep")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col2-178' 
+                            placeholder="00000-000"
+                            name="address.cep"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col5-466'/>
+                        <label about='complement'>Complemento</label>
+                        <input 
+                            {...register("address.complement")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col5-466' 
+                            name="address.complement"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-provider-field form-provider-col2-178'/>
+                        <label about='active'>Status</label>
+                        <input 
+                            {...register("active")}
+                            type="text" 
+                            className='form-control base-card form-provider-field form-provider-col2-178' 
+                            name="active"
+                        />
                     </div>
                 </div>
                 <div className='provider-crud-buttons-container'>
-                    <button className='btn providers-cancel-button'>CANCELAR</button>
+                    <button className='btn providers-cancel-button' onClick={handleCancel}>
+                        CANCELAR
+                    </button>
                     <button className='btn providers-save-button'>SALVAR</button>
                 </div>
             </form>
