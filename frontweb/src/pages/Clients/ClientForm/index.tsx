0c8c769b1e2b7a600177c1f9a42ebdc68968d112
Atila Silva <atila__7@hotmail.com>
2022-07-12 08:53:02 -0300

    Front - Clients Crud part 3

--- frontweb/src/pages/Clients/ClientForm/index.tsx
+++ frontweb/src/pages/Clients/ClientForm/index.tsx
@@ -1,52 +1,162 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { Client } from 'types/client';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const ClientForm = () => {
+
+    const history = useHistory();
+
+    const { register, handleSubmit, formState: { errors } } = useForm<Client>();
+
+    const onSubmit = (formData: Client) => {
+
+        const config: AxiosRequestConfig = {
+            method: 'POST',
+            url: "/clients",
+            data: formData,
+            withCredentials: true
+        }
+
+        requestBackend(config)
+            .then(response => {
+                console.log(response.data);
+            });
+    };
+
+    const handleCancel = () => {
+        history.replace("/clients");
+    }
+
     return (
         <div className="page-container form-client-page-container-especific">
             <div className="form-client-titulo-container">
                 <h1>Cadastro de Cliente</h1>
             </div>
-            <form action=''>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className='form-client-fields-container'>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col5-466' />
+                    <label about='contact'>Nome do Contato*</label>
+                        <input
+                            {...register("contact", {
+                                required: 'Campo obrigatório'
+                            })}
+                            type="text"
+                            className={`form-control base-card form-client-field form-client-col5-466 ${errors.contact ? 'is-invalid' : ''}`}
+                            placeholder="Ex: João Batista da Silva"
+                            name="contact"
+                        />
+                        <div className="invalid-feedback d-block">{errors.contact?.message}</div>
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col2-178' />
+                        <label about='cnpj'>CNPJ</label>
+                        <input
+                            {...register("cnpj")}
+                            type="text"
+                            className='form-control base-card form-client-field form-client-col2-178'
+                            placeholder="00.000.000/0000-00"
+                            name="cnpj"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col2-178' />
+                        <label about='cpf'>CPF</label>
+                        <input 
+                            {...register("cpf")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col2-178'
+                            placeholder="000.000.000-00"
+                            name="cpf"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col3-274' />
+                        <label about='corporateName'>Razão Social</label>
+                        <input 
+                            {...register("corporateName")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col3-274' 
+                            name="corporateName"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col5-466' />
+                        <label about='address.street'>Rua</label>
+                        <input 
+                            {...register("address.street")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col5-466' 
+                            name="address.street"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col1-82' />
+                        <label about='number'>Nº</label>
+                        <input 
+                            {...register("address.number")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col1-82' 
+                            name="address.number"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col3-274' />
+                        <label about='neighborhood'>Bairro</label>
+                        <input 
+                            {...register("address.neighborhood")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col3-274' 
+                            name="address.neighborhood"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col3-274' />
+                        <label about='city'>Cidade</label>
+                        <input 
+                            {...register("address.city")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col3-274' 
+                            name="address.city"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col3-274' />
+                        <label about='state'>Estado</label>
+                        <input 
+                            {...register("address.state")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col3-274' 
+                            name="address.state"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col2-178' />
+                    <label about='cep'>CEP</label>
+                        <input 
+                            {...register("address.cep")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col2-178' 
+                            placeholder="00000-000"
+                            name="address.cep"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col5-466' />
+                        <label about='complement'>Complemento</label>
+                        <input 
+                            {...register("address.complement")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col5-466' 
+                            name="address.complement"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-client-field form-client-col2-178' />
+                    <label about='active'>Status</label>
+                        <input 
+                            {...register("active")}
+                            type="text" 
+                            className='form-control base-card form-client-field form-client-col2-178' 
+                            name="active"
+                        />
                     </div>
                 </div>
                 <div className='client-crud-buttons-container'>
-                    <button className='btn clients-cancel-button'>CANCELAR</button>
+                    <button className='btn clients-cancel-button' onClick={handleCancel}>
+                        CANCELAR
+                    </button>
                     <button className='btn clients-save-button'>SALVAR</button>
                 </div>
             </form>
