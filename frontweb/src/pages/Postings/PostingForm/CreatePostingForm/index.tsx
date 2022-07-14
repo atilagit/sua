a6fd0dbab39049630b79f140bec0296c5027be01
Atila Silva <atila__7@hotmail.com>
2022-07-13 23:30:43 -0300

    Front - Postings Crud part 3

--- frontweb/src/pages/Postings/PostingForm/CreatePostingForm/index.tsx
+++ frontweb/src/pages/Postings/PostingForm/CreatePostingForm/index.tsx
@@ -1,43 +1,160 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { Posting } from 'types/posting';
+import { ShortEmployee } from 'types/shortEmployee'
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const CreatePostingForm = () => {
+
+    const empregado: ShortEmployee = {
+        "id": 1,
+        "name": ""
+    }
+
+    const history = useHistory();
+
+    const { register, handleSubmit, formState: { errors } } = useForm<Posting>();
+
+    const onSubmit = (formData: Posting) => {
+
+        const data = { ...formData, salaryAdvance: false }
+
+        const config: AxiosRequestConfig = {
+            method: 'POST',
+            url: "/postings",
+            data: data,
+            withCredentials: true
+        }
+
+        requestBackend(config)
+            .then(response => {
+                console.log(response.data);
+            });
+    };
+
+    const handleCancel = () => {
+        history.replace("/postings");
+    }
+
     return (
         <div className="page-container form-create-posting-page-container-especific">
-            <form action=''>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className='form-create-posting-fields-container'>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col3-274' />
+                        <label about='provider'>Fornecedor</label>
+                        <input
+                            {...register("provider.id")}
+                            type="text"
+                            className='form-control base-card form-create-posting-field form-create-posting-col3-274'
+                            name="provider"
+                            defaultValue={empregado.id}
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col3-274' />
+                        <label about='client'>Cliente</label>
+                        <input
+                            {...register("client.id")}
+                            type="text"
+                            className='form-control base-card form-create-posting-field form-create-posting-col3-274'
+                            name="client"
+                            defaultValue={empregado.id}
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col2-178' />
+                        <label about='date'>Data*</label>
+                        <input
+                            {...register("date", {
+                                required: 'Campo obrigatório'
+                            })}
+                            type='date'
+                            className={`form-control base-card form-create-posting-field form-create-posting-col2-178 ${errors.date ? 'is-invalid' : ''}`}
+                            placeholder="00.000.000/0000-00"
+                            name="date"
+                        />
+                        <div className="invalid-feedback d-block">{errors.date?.message}</div>
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col2-178' />
+                        <label about='unit'>Unidade*</label>
+                        <input 
+                            {...register("unit", {
+                                required: 'Campo obrigatório'
+                            })}
+                            type="text" 
+                            className={`form-control base-card form-create-posting-field form-create-posting-col2-178 ${errors.unit ? 'is-invalid' : ''}`} 
+                            name="unit"
+                        />
+                        <div className="invalid-feedback d-block">{errors.unit?.message}</div>
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col2-178' />
+                        <label about='price'>Preço*</label>
+                        <input 
+                            {...register("price", {
+                                required: 'Campo obrigatório'
+                            })}
+                            type="text" 
+                            className={`form-control base-card form-create-posting-field form-create-posting-col2-178 ${errors.price ? 'is-invalid' : ''}`} 
+                            name="price"
+                        />
+                        <div className="invalid-feedback d-block">{errors.price?.message}</div>
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col2-178' />
+                        <label about='employee'>Funcionário*</label>
+                        <input 
+                            {...register("employee.id", {
+                                required: 'Campo obrigatório'
+                            })}
+                            type="text" 
+                            className={`form-control base-card form-create-posting-field form-create-posting-col2-178 ${errors.employee?.id ? 'is-invalid' : ''}`} 
+                            name="employee"
+                            defaultValue={empregado.id}
+                        />
+                        <div className="invalid-feedback d-block">{errors.employee?.id?.message}</div>
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col2-178' />
+                        <label about='quantity'>Quantidade*</label>
+                        <input 
+                            {...register("quantity", {
+                                required: 'Campo obrigatório'
+                            })}
+                            type="text" 
+                            className={`form-control base-card form-create-posting-field form-create-posting-col2-178 ${errors.quantity ? 'is-invalid' : ''}`} 
+                            name="quantity"
+                        />
+                        <div className="invalid-feedback d-block">{errors.quantity?.message}</div>
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col2-178' />
+                        <label about='total'>Total</label>
+                        <input 
+                            type="text" 
+                            className='form-control base-card form-create-posting-field form-create-posting-col2-178' 
+                            name="total"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col5-466' />
+                        <label about='note'>Observação</label>
+                        <input 
+                            {...register("note")}
+                            type="text" 
+                            className='form-control base-card form-create-posting-field form-create-posting-col5-466'
+                            name="note"
+                        />
                     </div>
                     <div>
-                        <input type="text" className='form-control base-card form-create-posting-field form-create-posting-col1-82' />
+                        <label about='note'>Resolv.</label>
+                        <input 
+                            {...register("resolved")}
+                            type='text' 
+                            className='form-control base-card form-create-posting-field form-create-posting-col1-82' 
+                            name="resolved"
+                        />
                     </div>
                 </div>
                 <div className='create-posting-crud-buttons-container'>
-                    <button className='btn create-postings-cancel-button'>CANCELAR</button>
+                    <button className='btn create-postings-cancel-button' onClick={handleCancel}>
+                        CANCELAR
+                    </button>
                     <button className='btn create-postings-save-button'>SALVAR</button>
                 </div>
             </form>
--- frontweb/src/pages/Postings/PostingForm/CreateSalaryAdvanceForm/index.tsx
+++ frontweb/src/pages/Postings/PostingForm/CreateSalaryAdvanceForm/index.tsx
@@ -1,41 +1,135 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { Posting } from 'types/posting';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const CreateSalaryAdvanceForm = () => {
+
+    const history = useHistory();
+
+    const { register, handleSubmit, formState: { errors } } = useForm<Posting>();
+
+    const onSubmit = (formData: Posting) => {
+
+        const config: AxiosRequestConfig = {
+            method: 'POST',
+            url: "/postings",
+            data: formData,
+            withCredentials: true
+        }
+
+        requestBackend(config)
+            .then(response => {
+                console.log(response.data);
+            });
+    };
+
+    const handleCancel = () => {
+        history.replace("/postings");
+    }
+
     return (
         <div className="page-container form-create-salary-advance-page-container-especific">
-            <form action=''>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className='form-create-salary-advance-fields-container'>
                     <div className='form-create-salary-advance-first-line'>
                         <div>
-                            <input type="text" className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178' />
+                            <label about='date'>Data*</label>
+                            <input
+                                {...register("date", {
+                                    required: 'Campo obrigatório'
+                                })}
+                                type='date'
+                                className={`form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178 ${errors.date ? 'is-invalid' : ''}`}
+                                placeholder="00.000.000/0000-00"
+                                name="date"
+                            />
+                            <div className="invalid-feedback d-block">{errors.date?.message}</div>
                         </div>
                         <div>
-                            <input type="text" className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178' />
+                            <label about='unit'>Unidade*</label>
+                            <input 
+                                {...register("unit", {
+                                    required: 'Campo obrigatório'
+                                })}
+                                type="text" 
+                                className={`form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178 ${errors.unit ? 'is-invalid' : ''}`} 
+                                name="unit"
+                            />
+                            <div className="invalid-feedback d-block">{errors.unit?.message}</div>
                         </div>
                         <div>
-                            <input type="text" className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178' />
+                            <label about='price'>Preço*</label>
+                            <input 
+                                {...register("price", {
+                                    required: 'Campo obrigatório'
+                                })}
+                                type="text" 
+                                className={`form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178 ${errors.price ? 'is-invalid' : ''}`} 
+                                name="price"
+                            />
+                            <div className="invalid-feedback d-block">{errors.price?.message}</div>
                         </div>
                     </div>
                     <div className='form-create-salary-advance-second-line'>
                         <div>
-                            <input type="text" className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178' />
+                            <label about='employee'>Funcionário*</label>
+                            <input 
+                                {...register("employee.id", {
+                                    required: 'Campo obrigatório'
+                                })}
+                                type="text" 
+                                className={`form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178 ${errors.employee?.id ? 'is-invalid' : ''}`} 
+                                name="employee"
+                            />
+                            <div className="invalid-feedback d-block">{errors.employee?.id?.message}</div>
                         </div>
                         <div>
-                            <input type="text" className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178' />
+                            <label about='quantity'>Quantidade*</label>
+                            <input 
+                                {...register("quantity", {
+                                    required: 'Campo obrigatório'
+                                })}
+                                type="text" 
+                                className={`form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178 ${errors.quantity ? 'is-invalid' : ''}`} 
+                                name="quantity"
+                            />
+                            <div className="invalid-feedback d-block">{errors.quantity?.message}</div>
                         </div>
                         <div>
-                            <input type="text" className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178' />
+                            <label about='total'>Total</label>
+                            <input 
+                                type="text" 
+                                className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col2-178' 
+                                name="total"
+                            />
                         </div>
                         <div>
-                            <input type="text" className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col5-466' />
+                            <label about='note'>Observação</label>
+                            <input 
+                                {...register("note")}
+                                type="text" 
+                                className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col5-466'
+                                name="note"
+                            />
                         </div>
                         <div>
-                            <input type="text" className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col1-82' />
+                            <label about='note'>Resolv.</label>
+                            <input 
+                                {...register("resolved")}
+                                type='text' 
+                                className='form-control base-card form-create-salary-advance-field form-create-salary-advance-col1-82' 
+                                name="resolved"
+                            />
                         </div>
                     </div>
                 </div>
                 <div className='create-salary-advance-crud-buttons-container'>
-                    <button className='btn create-salary-advances-cancel-button'>CANCELAR</button>
+                    <button className='btn create-salary-advances-cancel-button' onClick={handleCancel}>
+                        CANCELAR
+                        </button>
                     <button className='btn create-salary-advances-save-button'>SALVAR</button>
                 </div>
             </form >
